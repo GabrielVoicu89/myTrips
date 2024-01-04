47a717a120b7bb1427c6ab4f6b85655f149bffe4
GabrielVoicu89 <g_the_reaper@yahoo.com>
2024-01-04 14:28:45 +0100

    map and geolocation

--- src/components/City.jsx
+++ src/components/City.jsx
@@ -1,7 +1,9 @@
 import { useParams } from "react-router-dom";
-import styles from "./City.module.css";
 import { useEffect } from "react";
 import { useCities } from "../contexts/CitiesContext";
+
+import styles from "./City.module.css";
+
 import Spinner from "./Spinner";
 import BackButton from "./BackButton";
 
--- src/pages/AppLayout.jsx
+++ src/pages/AppLayout.jsx
@@ -1,5 +1,5 @@
 import Map from "../components/Map";
-import Sidebar from "../components/SideBar";
+import Sidebar from "../components/Sidebar";
 
 import styles from "./AppLayout.module.css";
 
